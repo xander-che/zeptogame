bd81118c35ae0e69593cc2f5324de637d7efcd54
xander-che <83426030+xander-che@users.noreply.github.com>
2021-05-04 14:52:30 +0300

    Create game_dicts.py

---
+++ game_dicts.py
@@ -0,0 +1,19 @@
+todays_matches_links = {'first championship name': 'link to the first championship schedule of todays matches',
+                        ...,
+                        'last championship name': 'link to the last championship schedule of todays matches'}
+
+results_links = {'first championship name': 'link to the first championship statistics',
+                 ...,
+                 'last championship name': 'link to the last championship statistics'}
+
+champs_archive_data_paths = {'first championship name': 'path to the first championship dataset',
+                             ...,
+                             'last championship name': 'path to the last championship dataset'}
+
+map_champ_reduction = {'first championship name inside the application': 'first championship short name',
+                       ...,
+                       'last championship name inside the application': 'last championship short name'}
+
+map_champ_fullname = {'first championship name inside the application': 'first championship full name',
+                       ...,
+                       'last championship name inside the application': 'last championship full name'}
